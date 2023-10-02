f4329c6b1927f4c843d83d08118095e377b93e61
Antonio Trindade <atrindade@umov.me>
2023-10-02 19:59:09 -0300

    Adiciona multi browser

--- src/test/java/com/core/DriverFactory.java
+++ src/test/java/com/core/DriverFactory.java
@@ -4,6 +4,9 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
 	
@@ -11,10 +14,48 @@ public class DriverFactory {
 	
 	public static WebDriver getDriver() {
 		
-		if (driver == null) {		
-			System.setProperty("webdriver.chrome.driver", "/home/atrindade/Dev/drivers/chrome/chromedriver");		
-			driver = new ChromeDriver();	
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));			
+		// chrome / firefox / chrome-headless / firefox-headless		
+		String browser = GlobalProperty.getProperty("webdriver.test.browser");
+		String path = GlobalProperty.getProperty("webdriver.test.path");
+		
+		if (driver == null) {
+			
+			if (browser.equals("chrome")) {
+				
+				System.setProperty("webdriver.chrome.driver", path + "chromedriver");		
+				driver = new ChromeDriver();	
+								
+			}
+			else if (browser.equals("chrome-headless")) {
+				
+				System.setProperty("webdriver.chrome.driver", path + "chromedriver");
+				
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless");
+				options.addArguments("--windows-size=1200x960");
+				
+				driver = new ChromeDriver(options);
+				
+			}			
+			else if (browser.equals("firefox")) {
+				System.setProperty("webdriver.geckodriver.driver", path + "geckodriver");
+				driver = new FirefoxDriver();				
+				
+			}
+			else if (browser.equals("firefox-headless")) {
+				System.setProperty("webdriver.geckodriver.driver", path + "geckodriver");
+				
+				FirefoxOptions options = new FirefoxOptions();
+				options.addArguments("--headless");
+				options.addArguments("--windows-size=1200x960");
+				
+				driver = new FirefoxDriver(options);
+			}
+			else {
+				System.out.println("Driver não está sendo reconhecido");
+			}
+			
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		}
 		
 		return driver;
